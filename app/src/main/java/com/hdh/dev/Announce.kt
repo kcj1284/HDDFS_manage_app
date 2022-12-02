d20fcab9d326773f5eded30c7d3d823320c27cc3
kcj1284 <kcj1284@gmail.com>
2022-12-02 17:18:10 +0900

    [#11] 다크모드 적용 beta 공지사항 content 연결

--- app/src/main/java/com/hdh/dev/Announce.kt
+++ app/src/main/java/com/hdh/dev/Announce.kt
@@ -1,14 +1,24 @@
 package com.hdh.dev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.hdh.dev.databinding.ActivityAnnounceBinding
 
 class Announce : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_announce)
+    private lateinit var binding: ActivityAnnounceBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityAnnounceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.announce01Btn.setOnClickListener{
+            val intentAnnounce01 = Intent(this, AnnounceContent01::class.java)
+            startActivity(intentAnnounce01)
+        }
     }
 }
--- app/src/main/java/com/hdh/dev/SetApp.kt
+++ app/src/main/java/com/hdh/dev/SetApp.kt
@@ -2,10 +2,23 @@ package com.hdh.dev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.hdh.dev.databinding.ActivitySetAppBinding
 
 class SetApp : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySetAppBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_set_app)
+        binding = ActivitySetAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lightModeBtn.setOnClickListener{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+        binding.darkModeBtn.setOnClickListener{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
     }
 }
