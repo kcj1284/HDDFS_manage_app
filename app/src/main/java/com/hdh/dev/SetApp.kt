38c27fbfd6d1e36074394c0a3500291fc1a87e82
kcj1284 <kcj1284@gmail.com>
2022-12-02 09:40:49 +0900

    [#05] main화면 버튼 각 Activity 연결

---
+++ app/src/main/java/com/hdh/dev/SetApp.kt
@@ -0,0 +1,11 @@
+package com.hdh.dev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_app)
+    }
+}
