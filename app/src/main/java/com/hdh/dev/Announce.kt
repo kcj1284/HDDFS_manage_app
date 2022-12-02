46bf8933f4a4c2025e3e5aa234412984ed330fc1
DABIN222 <req134679@naver.com>
2022-12-02 16:03:57 +0900

    [#09] 공지사항, 공지사항 상세 페이지(수정 중)

--- app/src/main/java/com/hdh/dev/Announce.kt
+++ app/src/main/java/com/hdh/dev/Announce.kt
@@ -2,10 +2,13 @@ package com.hdh.dev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Announce : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_announce)
+
     }
 }
