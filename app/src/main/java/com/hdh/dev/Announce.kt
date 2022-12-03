02f5dc05dc7f2932bb95f4df6d4879e32324a5ba
parkyeomyeong <tldldh1212@naver.com>
2022-12-03 14:55:26 +0900

    공지사항 정신없는거 압축함

--- app/src/main/java/com/hdh/dev/Announce.kt
+++ app/src/main/java/com/hdh/dev/Announce.kt
@@ -3,7 +3,6 @@ package com.hdh.dev
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.hdh.dev.databinding.ActivityAnnounceBinding
 
 class Announce : AppCompatActivity() {
@@ -16,23 +15,28 @@ class Announce : AppCompatActivity() {
         binding = ActivityAnnounceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.announce01Btn.setOnClickListener{
-            val intentAnnounce01 = Intent(this, AnnounceContent01::class.java)
+            val intentAnnounce01 = Intent(this, AnnounceContent::class.java)
+            intentAnnounce01.putExtra("order", 1)
             startActivity(intentAnnounce01)
         }
 
         binding.announce02Btn.setOnClickListener {
-            val intentAnnounce02 = Intent(this, AnnounceContent02::class.java)
+            val intentAnnounce02 = Intent(this, AnnounceContent::class.java)
+            intentAnnounce02.putExtra("order", 2)
             startActivity(intentAnnounce02)
         }
 
         binding.announce03Btn.setOnClickListener {
-            val intentAnnounce03 = Intent(this, AnnounceContent03::class.java)
+            val intentAnnounce03 = Intent(this, AnnounceContent::class.java)
+            intentAnnounce03.putExtra("order", 3)
             startActivity(intentAnnounce03)
         }
 
         binding.announce04Btn.setOnClickListener {
-            val intentAnnounce04 = Intent(this, AnnounceContent04::class.java)
+            val intentAnnounce04 = Intent(this, AnnounceContent::class.java)
+            intentAnnounce04.putExtra("order", 4)
             startActivity(intentAnnounce04)
         }
     }
