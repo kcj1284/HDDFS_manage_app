1f1368b8d1f4ef943de4590c639924ffe7269a28
gahee3839 <gahee3839@naver.com>
2022-12-08 11:36:54 +0900

    [#54-2]push

--- app/src/main/java/com/hdh/dev/QrSearch.kt
+++ app/src/main/java/com/hdh/dev/QrSearch.kt
@@ -136,7 +136,6 @@ class QrSearch : AppCompatActivity() {
                                 binding.errorText.text = "상품이 존재하지 않습니다."
                             }
                             isDetected = false
-                            //Toast.makeText(this@QrSearch,"상품이 존재하지 않습니다.",Toast.LENGTH_LONG)
                         }
 
                     }.start()
