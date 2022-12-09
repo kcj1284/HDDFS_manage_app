7034cef5b7d14ef39cf40c12d61db8bbb86a3e78
DABIN222 <req134679@naver.com>
2022-12-09 21:12:49 +0900

    [#65] 전체 레이아웃 수정 완료

--- app/src/main/java/com/hdh/dev/StartLogoActivity.kt
+++ app/src/main/java/com/hdh/dev/StartLogoActivity.kt
@@ -3,14 +3,27 @@ package com.hdh.dev
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class StartLogoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_logo)
 
-        val nextIntent = Intent(this, StartActivity::class.java)
+/*        val nextIntent = Intent(this, StartActivity::class.java)
         startActivity(nextIntent)
-        finish()
+        finish()*/
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            // 일정 시간이 지나면 StartActivity 로 이동
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+
+            finish()
+
+        }, 2000) // 2초 이후 실행
     }
+
 }
